dc0718584a53bcfcd57768338cf88c6dd8924ddf
James Jenkins <james.jenkins@dvcorporate.com>
2022-02-15 12:17:18 +0000

    test

---
+++ models/vivastreet_gb/feeds.sql
@@ -0,0 +1,30 @@
+
+/*
+    Welcome to your first dbt model!
+    Did you know that you can also configure models directly within SQL files?
+    This will override configurations stated in dbt_project.yml
+
+    Try changing "table" to "view" below
+*/
+
+{{ config(
+    materialized='incremental',
+    unique_key='_airbyte_unique_key'
+    )
+}}
+
+with source_data as (
+
+    select _airbyte_unique_key, cast(id as integer), 'gb' as country, notes, enabled, ad_status, created_at, updated_at, description, repost_only, template_name, upload_credential_id
+    from vivastreet_production.gb_feed
+
+)
+
+select *
+from source_data
+
+/*
+    Uncomment the line below to remove records with null `id` values
+*/
+
+-- where id is not null
