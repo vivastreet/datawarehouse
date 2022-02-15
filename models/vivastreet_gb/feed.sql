c3b5df554dfd7798c85ffafb51740960d355e477
James Jenkins <james.jenkins@dvcorporate.com>
2022-02-15 15:06:56 +0000

    test

--- models/vivastreet_gb/feed.sql
+++ models/vivastreet_gb/feed.sql
@@ -9,13 +9,13 @@
 
 {{ config(
     materialized='incremental',
-    unique_key='_airbyte_unique_key'
+    unique_key='_airbyte_ab_id'
     )
 }}
 
 with source_data as (
 
-    select _airbyte_unique_key, cast(id as integer) as id, 'gb' as country, notes, enabled, ad_status, created_at, updated_at, description, repost_only, template_name, upload_credential_id
+    select _airbyte_ab_id, cast(id as integer) as id, 'gb' as country, notes, enabled, ad_status, created_at, updated_at, description, repost_only, template_name, upload_credential_id
     from vivastreet_production.gb_feed
 
 )
