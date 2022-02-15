3ec458d75eb7515889697a9eca575cf01dade39a
James Jenkins <james.jenkins@dvcorporate.com>
2022-02-15 17:24:10 +0000

    test

--- models/vivastreet_production/payment_solution_orders.sql
+++ models/vivastreet_production/payment_solution_orders.sql
@@ -6,8 +6,8 @@
 
 with source_data as (
 
-    select _airbyte_ab_id, cast(id as integer) as id, 'gb' as country, user_id, classified_id, plan_ids, plan_types, cast(transaction_id as integer) as transaction_id, provider_id, result_code, refunded, processed_at, created_at, updated_at, cast(amount as NUMERIC) as amount, currency, status, denialid, cast(service_fee as NUMERIC) as service_fee, payment_method, boleto_url
-    from vivastreet_production.gb_payment_solution_orders
+    select _airbyte_ab_id, cast(id as integer) as id, '{{ var("country") }}' as country, user_id, classified_id, plan_ids, plan_types, cast(transaction_id as integer) as transaction_id, provider_id, result_code, refunded, processed_at, created_at, updated_at, cast(amount as NUMERIC) as amount, currency, status, denialid, cast(service_fee as NUMERIC) as service_fee, payment_method, boleto_url
+    from vivastreet_production.{{ var("country") }_payment_solution_orders
 
 )
 
