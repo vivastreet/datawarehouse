4f5d4088be5200cf75c2ede1ec016e022e439980
James Jenkins <james.jenkins@dvcorporate.com>
2022-02-17 16:28:16 +0000

    test

--- models/vivastreet_production/invoices.sql
+++ models/vivastreet_production/invoices.sql
@@ -6,7 +6,7 @@
 
 with source_data as (
 
-    select _airbyte_ab_id, cast(id as integer) as id, cast(vat as numeric) as vat, cast(sent as datetime) as sent, email, items, cast(total as numeric) as total, country, cast(created as datetime) as created, category, cast(order_id as integer) as order_id, cast(subtotal as numeric) as subtotal, processor_code, cast(vat_percentage as numeric) as vat_percentage
+    select _airbyte_ab_id, cast(id as integer) as id, cast(vat as numeric) as vat, cast(sent as timestamp) as sent, email, items, cast(total as numeric) as total, country, cast(created as timestamp) as created, category, cast(order_id as integer) as order_id, cast(subtotal as numeric) as subtotal, processor_code, cast(vat_percentage as numeric) as vat_percentage
     from vivastreet_production.common_invoices
     where created > '2019-01-01'
 )
