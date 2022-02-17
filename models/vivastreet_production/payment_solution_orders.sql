0ba3b5d7156272bfc360f95ce26c4e45764c4c55
James Jenkins <james.jenkins@dvcorporate.com>
2022-02-17 12:02:54 +0000

    test

---
+++ models/vivastreet_production/payment_solution_orders.sql
@@ -0,0 +1,21 @@
+
+{{ config(
+    unique_key='_airbyte_ab_id'
+    )
+}}
+
+with source_data as (
+
+    select _airbyte_ab_id, 'gb' as country, cast(id as integer) as id, cast(user_id as integer) as user_id, cast(classified_id as integer) as classified_id, plan_ids, plan_types, cast(transaction_id as integer) as transaction_id, provider_id, result_code, refunded, cast(processed_at as timestamp) as processed_at, cast(created_at as timestamp) as created_at, cast(updated_at as timestamp) as updated_at, cast(amount as numeric) as amount, currency, status, denialid, service_fee, payment_method, boleto_url
+    from vivastreet_production.gb_payment_solution_orders
+    where processed > '2019-01-01'
+)
+
+select *
+from source_data
+
+/*
+    Uncomment the line below to remove records with null `id` values
+*/
+
+-- where id is not null
