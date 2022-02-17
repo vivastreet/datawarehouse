4f162b2f9e780b8ea3e9e391464e75c0523ef109
James Jenkins <james.jenkins@dvcorporate.com>
2022-02-17 13:50:40 +0000

    test

--- models/vivastreet_production/payment_solution_orders.sql
+++ models/vivastreet_production/payment_solution_orders.sql
@@ -6,7 +6,7 @@
 
 with source_data as (
 
-    select _airbyte_ab_id, 'gb' as country, cast(id as integer) as id, cast(user_id as integer) as user_id, cast(classified_id as integer) as classified_id, plan_ids, plan_types, cast(transaction_id as integer) as transaction_id, provider_id, result_code, refunded, cast(processed_at as timestamp) as processed_at, cast(created_at as timestamp) as created_at, cast(updated_at as timestamp) as updated_at, cast(amount as numeric) as amount, currency, status, denialid, service_fee, payment_method, boleto_url
+    select _airbyte_ab_id, 'gb' as country, cast(id as integer) as id, cast(user_id as integer) as user_id, cast(classified_id as integer) as classified_id, plan_ids, plan_types, cast(transaction_id as integer) as transaction_id, provider_id, result_code, refunded, PARSE_DATETIME('%Y-%m-%d %H:%M:%S UTC', processed_at) as processed_at, PARSE_DATETIME('%Y-%m-%d %H:%M:%S UTC', created_at) as created_at, PARSE_DATETIME('%Y-%m-%d %H:%M:%S UTC', updated_at) as updated_at, cast(amount as numeric) as amount, currency, status, denialid, service_fee, payment_method, boleto_url
     from vivastreet_production.gb_payment_solution_orders
     where created_at > '2019-01-01'
 )
