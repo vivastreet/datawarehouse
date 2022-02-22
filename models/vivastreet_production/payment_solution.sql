5b2e189d090abfa31e4247a40a6fe48dbe65642f
James Jenkins <james.jenkins@dvcorporate.com>
2022-02-22 09:32:06 +0000

    test

--- models/vivastreet_production/payment_solution.sql
+++ models/vivastreet_production/payment_solution.sql
@@ -4,15 +4,15 @@
     )
 }}
 
-with source_data as (
+-- with source_data as (
 
-    select _airbyte_ab_id, cast(id as integer) as id, geo, cast(date as timestamp) as date, brand, cast(user_id as integer) as user_id, boleto_id, paid_date, client_cpf, plans_sold, client_name, sales_agent, client_email, cast(boleto_amount as numeric) as boleto_amount, cast(boleto_status as integer) as boleto_status, client_surname, sales_agent_email
-    from vivastreet_production.payment_suite_brcs_boleto_log
-    where date > '2019-01-01'
-)
+--     select _airbyte_ab_id, cast(id as integer) as id, geo, cast(date as timestamp) as date, brand, cast(user_id as integer) as user_id, boleto_id, paid_date, client_cpf, plans_sold, client_name, sales_agent, client_email, cast(boleto_amount as numeric) as boleto_amount, cast(boleto_status as integer) as boleto_status, client_surname, sales_agent_email
+--     from vivastreet_production.payment_solution_payments
+--     where date > '2019-01-01'
+-- )
 
-select *
-from source_data
+-- select *
+-- from source_data
 
 /*
     Uncomment the line below to remove records with null `id` values
