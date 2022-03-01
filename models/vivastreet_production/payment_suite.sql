416c2dc1e30204f040f66670ce32dcf307180818
luiscpereira <luis.pereira@dm-companies.com>
2022-03-01 09:36:17 +0000

    Adds payment suite orders country.

--- models/vivastreet_production/payment_suite.sql
+++ models/vivastreet_production/payment_suite.sql
@@ -6,7 +6,16 @@
 
 with source_data as (
 
-    select _airbyte_ab_id, cast(id as integer) as id, geo, cast(date as timestamp) as date, brand, cast(user_id as integer) as user_id, boleto_id, paid_date, client_cpf, plans_sold, client_name, sales_agent, client_email, cast(boleto_amount as numeric) as boleto_amount, cast(boleto_status as integer) as boleto_status, client_surname, sales_agent_email
+    select _airbyte_ab_id,
+    CASE
+        WHEN brand = 'vivastreet.co.uk' THEN 'gb'
+        WHEN brand = 'vivalocal.com'    THEN 'br'
+        WHEN brand = 'vivastreet.cl'    THEN 'cl'
+        WHEN brand = 'vivastreet.com'   THEN 'fr'
+        WHEN brand = 'vivastreet.be'    THEN 'be'
+        WHEN brand = 'vivastreet.it'    THEN 'it'
+    END country,
+    cast(id as integer) as id, geo, cast(date as timestamp) as date, brand, cast(user_id as integer) as user_id, boleto_id, paid_date, client_cpf, plans_sold, client_name, sales_agent, client_email, cast(boleto_amount as numeric) as boleto_amount, cast(boleto_status as integer) as boleto_status, client_surname, sales_agent_email
     from vivastreet_production.payment_suite_brcs_boleto_log
     where date > '2019-01-01'
 )
