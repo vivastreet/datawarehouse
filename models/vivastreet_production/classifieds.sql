636b0e7415ca6267a1dcdc86185bd42fa6b2772a
luiscpereira <luis.pereira@dm-companies.com>
2022-03-01 12:50:18 +0000

    Adds back user_id

--- models/vivastreet_production/classifieds.sql
+++ models/vivastreet_production/classifieds.sql
@@ -6,10 +6,10 @@
 }}
 
 with source_data as (
-    select _airbyte_ab_id, 'gb' as country, cast(id as integer) as id, link, cast(cat_1 as integer) as cat_1, cast(cat_2 as integer) as cat_2, cast(cat_3 as integer) as cat_3, cast(cat_4 as integer) as cat_4, cast(cat_5 as integer) as cat_5, active, cast(posted as timestamp) as posted, status, cast(created as timestamp) as created, phone_nbr
+    select _airbyte_ab_id, 'gb' as country, cast(id as integer) as id, CAST(user_id as integer), link, cast(cat_1 as integer) as cat_1, cast(cat_2 as integer) as cat_2, cast(cat_3 as integer) as cat_3, cast(cat_4 as integer) as cat_4, cast(cat_5 as integer) as cat_5, active, cast(posted as timestamp) as posted, status, cast(created as timestamp) as created, phone_nbr
     from vivastreet_production.gb_classifieds
     UNION ALL
-    select _airbyte_ab_id, 'gb' as country, cast(id as integer) as id, link, cast(cat_1 as integer) as cat_1, cast(cat_2 as integer) as cat_2, cast(cat_3 as integer) as cat_3, cast(cat_4 as integer) as cat_4, cast(cat_5 as integer) as cat_5, active, cast(posted as timestamp) as posted, status, cast(created as timestamp) as created, phone_nbr
+    select _airbyte_ab_id, 'gb' as country, cast(id as integer) as id, CAST(user_id as integer), link, cast(cat_1 as integer) as cat_1, cast(cat_2 as integer) as cat_2, cast(cat_3 as integer) as cat_3, cast(cat_4 as integer) as cat_4, cast(cat_5 as integer) as cat_5, active, cast(posted as timestamp) as posted, status, cast(created as timestamp) as created, phone_nbr
     from vivastreet_production.archive_gb
 )
 
