89af5f63df71a06e94c10025f558567c48e70b1f
luiscpereira <luis.pereira@dm-companies.com>
2022-03-01 14:59:05 +0000

    Adds featured_plans_durations

---
+++ models/vivastreet_production/featured_plans_type_durations.sql
@@ -0,0 +1,15 @@
+
+with source_data as (
+SELECT plan.country, price, type, plan.plan_id, duration.seconds, plan_type.name FROM {{ ref('featured_plans') }} plan
+    LEFT JOIN {{ ref('featured_plan_type') }} plan_type ON (plan.featured_plan_type_id = plan_type.id AND plan.country = plan_type.country)
+    LEFT JOIN {{ ref('featured_durations') }} duration ON (duration.duration_id = plan.duration_id AND duration.country = plan.country)
+)
+
+select *
+from source_data
+
+/*
+Uncomment the line below to remove records with null `id` values
+*/
+
+-- where id is not null
