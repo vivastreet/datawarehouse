4cc4bc8c7d9ad4707c54cd6cf164466acf871ecb
luiscpereira <luis.pereira@dm-companies.com>
2022-03-03 13:21:00 +0000

    Adds more fields to autopilot.

--- models/vivastreet_production/autopilot.sql
+++ models/vivastreet_production/autopilot.sql
@@ -5,7 +5,23 @@
 }}
 
 with source_data as (
-    select _airbyte_ab_id, JSON_EXTRACT_SCALAR(noiredata, '$.id') as noire_id, cast(id as integer) as id, 'gb' as country, cast(date as timestamp) as date, flag, cast(amount as numeric) as amount, method, noiredata
+    select _airbyte_ab_id, 
+    cast(id as integer) as id, 
+    'gb' as country, 
+    cast(date as timestamp) as date, flag, 
+    cast(amount as numeric) as amount, 
+    method, 
+    noiredata,
+    JSON_EXTRACT_SCALAR(noiredata, '$.id') as noire_id, 
+    JSON_EXTRACT_SCALAR(noiredata, "$.customParameters.SHOPPER_plan") as plan,
+    JSON_EXTRACT_SCALAR(noiredata, "$.customParameters.SHOPPER_userId") as userId,
+    JSON_EXTRACT_SCALAR(noiredata, "$.customParameters.SHOPPER_EndToEndIdentity") as EndToEndIdentity,
+    JSON_EXTRACT_SCALAR(noiredata, "$.customParameters.SHOPPER_location") as location,
+    JSON_EXTRACT_SCALAR(noiredata, "$.customParameters.SHOPPER_userName") as userName,
+    JSON_EXTRACT_SCALAR(noiredata, "$.customParameters.SHOPPER_category") as category,
+    JSON_EXTRACT_SCALAR(noiredata, "$.customParameters.SHOPPER_email") as email,
+    JSON_EXTRACT_SCALAR(noiredata, "$.customParameters.SHOPPER_token") as token,
+    JSON_EXTRACT_SCALAR(noiredata, "$.customParameters.SHOPPER_adId") as adId,
     FROM vivastreet_production.autopilot_gb_payments
 )
 
