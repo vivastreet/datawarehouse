b8052c6a229f82c0693b3c31ec21ab85733d484f
luiscpereira <luis.pereira@dm-companies.com>
2022-03-03 13:05:34 +0000

    Autopilot adds Noire id.

--- models/vivastreet_production/autopilot.sql
+++ models/vivastreet_production/autopilot.sql
@@ -5,13 +5,11 @@
 }}
 
 with source_data as (
-
-    select _airbyte_ab_id, cast(id as integer) as id, 'gb' as country, cast(date as timestamp) as date, flag, cast(amount as numeric) as amount, method, noiredata
-    from vivastreet_production.autopilot_gb_payments
-
+    select _airbyte_ab_id, JSON_EXTRACT_SCALAR(noiredata, '$.id') as noire_id, cast(id as integer) as id, 'gb' as country, cast(date as timestamp) as date, flag, cast(amount as numeric) as amount, method, noiredata
+    FROM vivastreet_production.autopilot_gb_payments
 )
 
-SELECT *  FROM source_data
+SELECT * FROM source_data
 
 /*
     Uncomment the line below to remove records with null `id` values
