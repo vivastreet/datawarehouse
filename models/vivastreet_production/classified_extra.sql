d9fc25924479b837231544bb8fc6c72558e3c7d2
luiscpereira <luis.pereira@dm-companies.com>
2022-04-06 23:09:17 +0100

    Fixes

--- models/vivastreet_production/classified_extra.sql
+++ models/vivastreet_production/classified_extra.sql
@@ -57,7 +57,6 @@ with source_data as (
     select _airbyte_ab_id, 'us' as country, email, cast(amount as numeric) as amount, cast(clad_id as integer) as clad_id, init_by, plan_id, cast(user_id as integer) as user_id, zipcode, cast(order_id as integer) as order_id, platform, cast(processed as timestamp) as processed, proxy_type, renew_type, cast(location_id as integer) as location_id, subcat_code, partner_name, customer_name, discount_type, posting_status, proxy_username, cast(clad_revenue_id as integer) as clad_revenue_id, existing_status, individual_type, transaction_type, cast(clad_affiliate_id as integer) as clad_affiliate_id, cast(user_affiliate_id as integer) as user_affiliate_id
             from data_lake.us_clad_revenue
             where processed > '2019-01-01'
-            UNION ALL
 )
 
 SELECT k.*
