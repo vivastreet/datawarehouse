2c16ab96cf9baa225cde82b917993e1d2daed5a2
luiscpereira <luis.pereira@dm-companies.com>
2022-05-16 09:54:14 +0100

    Fix exchange rates

--- models/vivastreet_production/exchange_rates.sql
+++ models/vivastreet_production/exchange_rates.sql
@@ -6,5 +6,3 @@ with source_data as (
 
 SELECT k.*
 FROM ( SELECT ARRAY_AGG(x LIMIT 1)[OFFSET(0)] k  FROM source_data x GROUP BY date,country,currency_name,currency_rate)
-
-date,country,currency_name,currency_rate
